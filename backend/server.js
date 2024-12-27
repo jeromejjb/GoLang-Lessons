703e75a81438ee2fd77ba6cb8d5133d57734bae5
jeromejjb <jbreview@outlook.com>
2024-12-27 13:52:01 -0600

    fixing vercel behavior

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const { exec } = require('child_process');
-const cors = require('cors');
-const app = express();
-const port = 3001;
+// const express = require('express');
+// const { exec } = require('child_process');
+// const cors = require('cors');
+// const app = express();
+// const port = 3001;
 
-// Enable CORS
-app.use(cors());
-app.use(express.json());
+// // Enable CORS
+// app.use(cors());
+// app.use(express.json());
 
-app.post('/run-go', (req, res) => {
-  const goCode = req.body.code;
+// app.post('/run-go', (req, res) => {
+//   const goCode = req.body.code;
 
-  const fs = require('fs');
-  fs.writeFileSync('temp.go', goCode);
+//   const fs = require('fs');
+//   fs.writeFileSync('temp.go', goCode);
 
-  exec('go run temp.go', (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: stderr });
-    }
-    res.json({ output: stdout });
-  });
-});
+//   exec('go run temp.go', (error, stdout, stderr) => {
+//     if (error) {
+//       return res.status(500).json({ error: stderr });
+//     }
+//     res.json({ output: stdout });
+//   });
+// });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
